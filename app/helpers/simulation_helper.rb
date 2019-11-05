90f44bc793dd37ef6eca1497c10818883d85a65f
Krzysiek <potepa3@wp.pl>
2019-11-05 21:55:27 +0100

    improve form tags

--- app/helpers/simulation_helper.rb
+++ app/helpers/simulation_helper.rb
@@ -1,16 +1,39 @@
 module SimulationHelper
-	def errors_for(form, field)
+	
+    def form_field_for(form, field, opts={}, &block)
+        add_label = !opts.fetch(:label, nil).nil?
+        add_help = !opts.fetch(:help, nil).nil?
+
+        has_errors = form.object.errors[field].present?
+        _class = "form-control #{'is-invalid' if has_errors}"
+
+        content_tag :div, class: opts.fetch(:class, nil) do
+            concat form.label(field, opts.fetch(:label)) if add_label
+            concat capture(_class, &block)
+            concat help_for(field, opts.fetch(:help)) if add_help
+            concat errors_for(form, field)
+        end
+    end
+
+    def errors_for(form, field)
         content_tag(:p, form.object.errors[field].try(:first), class: 'form-text text-danger', id: "#{field}_error")
     end
 
-    def form_group_for(form, field, opts={}, &block)
-        label = opts.fetch(:label) { true }
-        has_errors = form.object.errors[field].present?
+    def help_for(field, help)
+        content_tag(:small, help, class: 'form-text text-muted', id: "#{field}_help")
+    end
 
-        content_tag :div, class: "form-group #{'has-error' if has_errors}" do
-            concat form.label(field, class: 'control-label') if label
-            concat capture(&block)
-            concat errors_for(form, field)
+    def form_elem(form, field, opts={}, &block)
+        _aclass = 'a'
+        content_tag :div, class: "form-group" do
+            concat form.label(field, class: 'control-label') 
+            concat capture(_aclass, &block)
+        end
+    end
+
+    def ccc ()
+        content_for :m_text_field do
+            text_field(:name, :class=>"#{_aclass}")
         end
     end
 end
